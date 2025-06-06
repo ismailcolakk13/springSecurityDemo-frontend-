0a86dd2ee16839490e21e420e41bb2fa4225e178
İsmail Çolak <ismail.colak@ogr.iuc.edu.tr>
2025-06-06 07:15:10 +0300

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { UserProvider } from "./context/UserContext.jsx";
 import "./style.css";
 
 createRoot(document.getElementById("root")).render(
   
-    <BrowserRouter>
+    <HashRouter>
       <UserProvider>
         <App />
       </UserProvider>
-    </BrowserRouter>
+    </HashRouter>
 
 );
 
